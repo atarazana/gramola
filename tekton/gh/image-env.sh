ce98a758113266bc1b50ca42cd07d553bde48e38
cvicens <carlos.vicens.alonso@gmail.com>
2021-08-03 17:02:48 +0200

    util images added

---
+++ tekton/gh/image-env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export REGISTRY=quay.io
+export REGISTRY_USER_ID=atarazana
+export IMAGE_NAME=gh
+export IMAGE_VERSION=0.0.1
+export FROM_IMAGE=registry.redhat.io/ubi8/ubi-minimal:latest
