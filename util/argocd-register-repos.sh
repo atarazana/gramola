aa48314bb7d10ba9190d94a750729c39bbc1484c
cvicens <carlos.vicens.alonso@gmail.com>
2021-08-12 08:56:18 +0200

    param reading logic added

--- util/argocd-register-repos.sh
+++ util/argocd-register-repos.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-GIT_USERNAME=cvicens
-# Ue this for gitlab
-# GIT_USER=git
-GIT_BASE_URL=https://github.com/atarazana/gramola
+echo "Configuration Repo URL (DON'T add .git at the end): " && read GIT_BASE_URL
+echo "GIT_BASE_URL=${GIT_BASE_URL}"
+if [ -z "${GIT_BASE_URL}" ]; then
+    echo "You have to provide the url of the configuration repo"
+    exit 1
+fi
+
+echo "User for ${GIT_BASE_URL} (type git if Gitlab): " && read GIT_USERNAME
+echo "GIT_USERNAME=${GIT_USERNAME}"
+if [ -z "${GIT_USERNAME}" ]; then
+    echo "You have to provide the user of the configuration repo"
+    exit 1
+fi
 
 # IF git repo is NOT secure uncomment
 # argocd repo add ${GIT_BASE_URL}.git
@@ -12,6 +21,12 @@ GIT_BASE_URL=https://github.com/atarazana/gramola
 # IF git repo is secure uncomment
 echo "Token for ${GIT_BASE_URL}: " && read -s GIT_TOKEN
 echo "GIT_TOKEN=${GIT_TOKEN}"
+if [ -z "${GIT_TOKEN}" ]; then
+    echo "You have to provide a Personal Access Token to access the configuration repo"
+    exit 1
+fi
+
+# Register
 argocd repo add ${GIT_BASE_URL}.git --username ${GIT_USERNAME} --password $GIT_TOKEN --upsert
-argocd repo add ${GIT_BASE_URL}-events.git --username ${GIT_USERNAME} --password $GIT_TOKEN --upsert
+#argocd repo add ${GIT_BASE_URL}-events.git --username ${GIT_USERNAME} --password $GIT_TOKEN --upsert
 
