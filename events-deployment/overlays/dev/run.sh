0613fd4285b5d01269bd68a02322bf8973d9e08b
cvicens <carlos.vicens.alonso@gmail.com>
2021-10-26 17:17:26 +0200

    namespaceSuffix on the names of the Application objects

--- events-deployment/overlays/dev/run.sh
+++ events-deployment/overlays/dev/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ARGOCD_APP_NAME=events
-#DEBUG="true"
 BASE_REPO_URL=https://github.com/atarazana/gramola
-helm template ../../helm_base --name-template $ARGOCD_APP_NAME --set debug=${DEBUG},baseRepoUrl=${BASE_REPO_URL} --include-crds > ../../helm_base/all.yml && kustomize build
+NAMESPACE_SUFFIX="-user1"
+helm template ../../helm_base --name-template $ARGOCD_APP_NAME --set debug=${DEBUG},baseRepoUrl=${BASE_REPO_URL} --include-crds > ../../helm_base/all.yml && kustomize build | sed "s/\(namespace:[[:space:]]\{1,\}\)\(gramola-.*\)/\1\2${NAMESPACE_SUFFIX}/"
