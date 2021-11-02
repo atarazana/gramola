9a1ab46f77eb35b0d3850206247d32b84ee0e88a
cvicens <carlos.vicens.alonso@gmail.com>
2021-11-02 11:49:35 +0100

    main -> .Values.gitRevision => gitRevision: github

--- util/argocd-login.sh
+++ util/argocd-login.sh
@@ -4,4 +4,4 @@ export ARGOCD_USERNAME=admin
 export ARGOCD_SERVER=$(oc get route/openshift-gitops-server -o jsonpath='{.status.ingress[0].host}' -n openshift-gitops)
 export ARGOCD_PASSWORD=$(oc get secret openshift-gitops-cluster -o jsonpath='{.data.admin\.password}' -n openshift-gitops | base64 -d)
 
-argocd login $ARGOCD_SERVER --insecure --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD
+argocd login $ARGOCD_SERVER --insecure --grpc-web --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD
