37a13da0405881be0e84ff436b8b7ff82503ac7d
cvicens <carlos.vicens.alonso@gmail.com>
2021-11-12 11:41:50 +0100

    adding routes for gitea

--- apps/cicd/run.sh
+++ apps/cicd/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ARGOCD_APP_NAME=gramola-events
-helm template . --name-template ${ARGOCD_APP_NAME} --include-crds
+BASE_REPO_URL=https://repository-gitea-system.apps.cluster-e608.e608.sandbox465.opentlc.com/user1/gramola
+NAMESPACE_SUFFIX="-user1"
+helm template . --name-template ${ARGOCD_APP_NAME} --set baseRepoUrl=${BASE_REPO_URL},username=${USERNAME},namespaceSuffix=${NAMESPACE_SUFFIX} --include-crds
