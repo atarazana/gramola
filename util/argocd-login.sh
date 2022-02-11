af5aef47462e4b4ffe6db7b6c6d61ee8f5bb317f
cvicens <carlos.vicens.alonso@gmail.com>
2022-02-11 12:24:05 +0100

    login with OAuth instead of

--- util/argocd-login.sh
+++ util/argocd-login.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-export ARGOCD_USERNAME=admin
-export ARGOCD_SERVER=$(oc get route/openshift-gitops-server -o jsonpath='{.status.ingress[0].host}' -n openshift-gitops)
-export ARGOCD_PASSWORD=$(oc get secret openshift-gitops-cluster -o jsonpath='{.data.admin\.password}' -n openshift-gitops | base64 -d)
 
-argocd login $ARGOCD_SERVER --insecure --grpc-web --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD
+export ARGOCD_HOST=$(oc get route/openshift-gitops-server -o jsonpath='{.status.ingress[0].host}' -n openshift-gitops)
+
+#export ARGOCD_USERNAME=admin
+#export ARGOCD_PASSWORD=$(oc get secret openshift-gitops-cluster -o jsonpath='{.data.admin\.password}' -n openshift-gitops | base64 -d)
+
+#argocd login $ARGOCD_HOST --insecure --grpc-web --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD
+
+argocd login $ARGOCD_HOST --insecure --grpc-web --sso
