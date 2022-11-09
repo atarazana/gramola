0fb0fcf08ac9b2e54272a9001e72a8c9be7f5a88
cvicens <carlos.vicens.alonso@gmail.com>
2022-11-09 16:09:22 +0100

    interim cicd-jenkins

--- apps/cicd-jenkins/ose-jenkins-agent-maven-custom/scripts/update-overlays.sh
+++ apps/cicd-jenkins/ose-jenkins-agent-maven-custom/scripts/update-overlays.sh
@@ -1,66 +1,60 @@
 #!/bin/sh
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
-    echo "Provide OVERLAYS_PATH SUB_OVERLAYS_TO_UPDATE OVERLAY NEW_DIGEST"
-    echo "$0 ./arco-saer-deployment/overlays 'L01,L02' dev XYZ"
+    echo "Provide OVERLAYS_PATH OVERLAY IMAGE_NAME NEW_NAME NEW_TAG"
+    echo "$0 ./events-deployment/overlays dev XYZ"
     exit 1;
 fi
 
 
-# OVERLAYS_PATH="./arco-saer-deployment/overlays"
-# SUB_OVERLAYS_TO_UPDATE="L01 L02"
-# OVERLAY="dev"
-# NEW_VALUE=
-
 OVERLAYS_PATH=${1}
-SUB_OVERLAYS_TO_UPDATE=${2//,/ }
-OVERLAY=${3}
-NEW_VALUE=${4}
+OVERLAY=${2}
+IMAGE_NAME=${3}
+NEW_NAME=${4}
+NEW_TAG=${5}
+
+echo "OVERLAYS_PATH=${OVERLAYS_PATH}"
+echo "OVERLAY=${OVERLAY}"
+echo "IMAGE_NAME=${IMAGE_NAME}"
+echo "NEW_NAME=${NEW_NAME}"
+echo "NEW_TAG=${NEW_TAG}"
 
 FILE_NAME="kustomization.yml"
 TWIN_OVERLAY_SUFFIX="cloud"
-SELECT_EXPRESSION='(.images.[] | select(.name == "arco-saer*")).newTag'
-
+SELECT_EXPRESSION='(.images.[] | select(.name == "'"${IMAGE_NAME}"'"))'
 
 echo "SELECT_EXPRESSION=${SELECT_EXPRESSION}"
-echo "NEW_VALUE=${NEW_VALUE}"
-echo "SUB_OVERLAYS_TO_UPDATE=${SUB_OVERLAYS_TO_UPDATE}"
 
-for SUB_OVERLAY in ${SUB_OVERLAYS_TO_UPDATE}
-do
-    FILE_TO_UPDATE="${OVERLAYS_PATH}/${OVERLAY}/${SUB_OVERLAY}/${FILE_NAME}"
-    echo "FILE_TO_UPDATE=${FILE_TO_UPDATE}"
+FILE_TO_UPDATE="${OVERLAYS_PATH}/${OVERLAY}/${FILE_NAME}"
+echo "FILE_TO_UPDATE=${FILE_TO_UPDATE}"
 
-    if [ ! -f ${FILE_TO_UPDATE} ]; then
+if [ ! -f ${FILE_TO_UPDATE} ]; then
     echo "======== ERROR ========"
     echo "File not found, aborting"
     ls -ltrh
     exit 1;
-    fi;
+fi;
 
-    # echo "======== BEFORE CHANGES ========"
-    # cat ${FILE_TO_UPDATE}
+# echo "======== BEFORE CHANGES ========"
+# cat ${FILE_TO_UPDATE}
 
-    # Update target with new value
-    yq -i eval '(.images.[] | select(.name == "arco-saer*")).newTag |= "'"${NEW_VALUE}"'"' ${FILE_TO_UPDATE}
-    
-    # echo "======== AFTER CHANGES ========"
-    # cat ${FILE_TO_UPDATE}
-done
+# Update target with new value
+yq -i eval ''"${SELECT_EXPRESSION}"'.newName |= "'"${NEW_NAME}"'"' ${FILE_TO_UPDATE}
+yq -i eval ''"${SELECT_EXPRESSION}"'.newTag |= "'"${NEW_TAG}"'"' ${FILE_TO_UPDATE}
 
-# If twin env
-for SUB_OVERLAY in ${SUB_OVERLAYS_TO_UPDATE}
-do
-    FILE_TO_UPDATE_TWIN="${OVERLAYS_PATH}/${OVERLAY}-${TWIN_OVERLAY_SUFFIX}/${SUB_OVERLAY}/${FILE_NAME}"
-    echo "FILE_TO_UPDATE_TWIN=${FILE_TO_UPDATE_TWIN}"
+# echo "======== AFTER CHANGES ========"
+# cat ${FILE_TO_UPDATE}
 
-    if [ -f ${FILE_TO_UPDATE_TWIN} ]; then
+FILE_TO_UPDATE_TWIN="${OVERLAYS_PATH}/${OVERLAY}-${TWIN_OVERLAY_SUFFIX}/${FILE_NAME}"
+echo "FILE_TO_UPDATE_TWIN=${FILE_TO_UPDATE_TWIN}"
+
+if [ -f ${FILE_TO_UPDATE_TWIN} ]; then
     echo "======== UPDATING TWIN OVERLAY ========"
     # Update target with new value
-    yq -i eval '(.images.[] | select(.name == "arco-saer*")).newTag |= "'"${NEW_VALUE}"'"' ${FILE_TO_UPDATE_TWIN}
+    yq -i eval ''"${SELECT_EXPRESSION}"'.newName |= "'"${NEW_NAME}"'"' ${FILE_TO_UPDATE_TWIN}
+    yq -i eval ''"${SELECT_EXPRESSION}"'.newTag |= "'"${NEW_TAG}"'"' ${FILE_TO_UPDATE_TWIN}
     else
     echo "TWIN OVERLAY NOT FOUND!"
-    fi;
-done
+fi;
 
 #echo -n "true" > $(results.SUCCESS)
