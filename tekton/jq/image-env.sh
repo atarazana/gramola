0045fd75fe31f9bc1edce8c042c0ef2a7542aeb8
cvicens <carlos.vicens.alonso@gmail.com>
2023-05-23 15:12:24 +0200

    added helm cli

--- tekton/jq/image-env.sh
+++ tekton/jq/image-env.sh
@@ -3,5 +3,5 @@
 export REGISTRY=quay.io
 export REGISTRY_USER_ID=atarazana
 export IMAGE_NAME=jq
-export IMAGE_VERSION=0.0.1
+export IMAGE_VERSION=0.0.3
 export FROM_IMAGE=registry.redhat.io/ubi8/ubi-minimal:latest
