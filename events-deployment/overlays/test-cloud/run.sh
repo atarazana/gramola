cb339dc8c8cbbc5b296df6c1502388e4563d8c0f
cvicens <carlos.vicens.alonso@gmail.com>
2023-09-04 10:53:22 +0200

    kustomized-helm -> gramola-kustomized-helm to avoid colliding with kitchensink if deployed together

--- events-deployment/overlays/test-cloud/run.sh
+++ events-deployment/overlays/test-cloud/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-ARGOCD_APP_NAME=economiacircular
-helm template ../../helm_base --name-template ${ARGOCD_APP_NAME} --include-crds > ../../helm_base/all.yml && kustomize build
+ARGOCD_APP_NAME=gramola-events
+ARGOCD_ENV_BASE_REPO_URL=https://github.com/atarazana/gramola
+ARGOCD_ENV_NAMESPACE_SUFFIX="-user1"
+helm template ../../helm_base --name-template $ARGOCD_APP_NAME --set debug=${ARGOCD_ENV_DEBUG},clusterName=${ARGOCD_ENV_DESTINATION_NAME},baseRepoUrl=${ARGOCD_ENV_BASE_REPO_URL},namespaceSuffix=${ARGOCD_ENV_NAMESPACE_SUFFIX} --include-crds > ../../helm_base/all.yml && kustomize build | sed "s/\(namespace:[[:space:]]\{1,\}\)\(gramola-.*\)/\1\2${ARGOCD_ENV_NAMESPACE_SUFFIX}/"
